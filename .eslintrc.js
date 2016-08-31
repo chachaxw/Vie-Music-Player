7b383812171c939dca218531196690e5fc3e7bee
chachaxw <867571123@qq.com>
2016-08-31 21:29:32 +0800

    add vuex

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   extends: 'airbnb-base',
   // required to lint *.vue files
   plugins: [
-    'html'
+    'html',
+    'vue'
   ],
   // add your custom rules here
   'rules': {
