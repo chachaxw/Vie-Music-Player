a0c9447f96d67181b02aa1853ab218ecd6036b2d
chachaxw <867571123@qq.com>
2016-08-31 23:53:23 +0800

    add PlayList component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
     <div class="logo">
-      <img src="./assets/images/logo.svg" width="100%" height="100%">
+      <img src="assets/images/logo.svg" width="100%" height="100%">
     </div>
+    <play-list></play-list>
   </div>
 </template>
 
 <script>
 import store from './store/store';
+import PlayList from 'components/PlayList.vue';
 
 export default {
   components: {
-
+    PlayList,
   },
   store,
 };
 </script>
 
 <style>
+  @import "assets/css/iconfont.css";
+
   html {
     height: 100%;
   }
@@ -34,6 +38,7 @@ export default {
     max-width: 800px;
     width: 100%;
     height: 500px;
+    position: relative;
     border-radius: 4px;
     background-color: rgba(255,255,255,1);
     box-shadow: 5px 40px 80px -30px rgba(219, 149, 68, 0.6);
---
+++ src/components/PlayList.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="play-list-btn">
+    <i class="iconfont icon-menu"></i>
+  </div>
+  <div class="music-play-list">
+    <h4>Play List</h4>
+    <ul>
+      <li class="flex-box" v-for="el in playList">
+        <div class="flex-item" v-text="el.songName"></div>
+        <div class="music-owner" v-text="el.singer">五月天</div>
+        <div class="music-duration" v-text="el.duration"></div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      playList: [
+        {
+          songName: 'OAOA',
+          singer: '五月天',
+          duration: '05:20',
+        },
+        {
+          songName: 'Four Seasons',
+          singer: '滨崎步',
+          duration: '04:20',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .play-list-btn {
+    position: absolute;
+    right: 20px;
+  }
+  .iconfont {
+    font-size: 22px;
+  }
+  .music-play-list {
+    width: 60%;
+    height: 414px;
+    position: absolute;
+    margin-top: 30px;
+    right: 0;
+  }
+  .music-play-list h4 {
+    background-color: #f5f5f5;
+    padding: 10px;
+    margin: 0;
+    color: #444;
+  }
+  .music-play-list ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  .music-play-list li:nth-child(2n) {
+    background-color: #f5f5f5;
+  }
+  .flex-box {
+    display: flex;
+    padding: 8px 16px;
+    width: 100%;
+    color: #444;
+    font-size: 13px;
+    line-height: 1;
+    text-align: left;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+  }
+  .flex-item {
+    flex: 1 auto;
+  }
+  .music-owner {
+    width: 160px;
+  }
+  .music-duration {
+    text-align: right;
+    width: 60px;
+  }
+</style>
